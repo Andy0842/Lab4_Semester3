99f689a3b85beccf9bcca8b7ab1a00c120fdd7c8
Yang Yue <202283890018@nuist.edu.cn>
2025-03-17 22:39:46 +0800

    Added ex1.py

---
+++ ex1.py
@@ -0,0 +1,7 @@
+#Enter a number and output twice of it.
+#Auther: Yang Yue
+
+# This program prompts the user to enter a number and then outputs its double
+number = float(input("Please enter a number: "))
+doubled_number = number * 2
+print(f"The double of {number} is {doubled_number}.")

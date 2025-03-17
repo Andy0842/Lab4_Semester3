d0fde0bf738369d1b03d4f0427bdab8ee1208946
Yang Yue <202283890018@nuist.edu.cn>
2025-03-17 22:48:45 +0800

    Added ex2.py

---
+++ ex2.py
@@ -0,0 +1,6 @@
+#Enter user's name and output it in uppercase
+#Auther: Yang Yue
+
+# This program prompts the user to enter their name and outputs it in uppercase
+name = input("Please enter your name: ")
+print(name.upper())
